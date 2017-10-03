41106966a415114113a9eec1f78c697bf436151c
UnVrai <287576470@qq.com>
2017-10-03 15:28:13 +0800

    1.0

---
+++ database/migrations/2017_09_17_145241_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->integer('id');
+            $table->char('name');
+            $table->char('number');
+            $table->integer('price');
+            $table->char('total');
+            $table->integer('actual');
+            $table->char('people');
+            $table->dateTime('create_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
